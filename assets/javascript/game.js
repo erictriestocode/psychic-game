5458c1c23187b2fdd9a53fb2d94e6fab2acd443a
erictriestocode <43325093+erictriestocode@users.noreply.github.com>
2018-12-10 21:17:35 -0600

    built out variables for game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,47 @@
 // Javascript file for Psychic game
 
+
 // User Guesses
 var userGuess;
 var computerGuess;
 // tracking wins/losses
 var wins = 0;
 var losses = 0;
-// tracking guesses
+// tracking user guesses
 var guessList = [];
-
-
 // computer choice array
-var compChoice = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z];
+var compChoice = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var guessNum = Math.floor(Math.random() * compChoice.length);
+var computerGuess = compChoice[guessNum];
+
+// console logs for testing variables
+console.log(computerGuess);
+console.log(guessNum);
+
+function game(){
+
+    
+
+}
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//reload page when complete per: https://stackoverflow.com/questions/16763665/html5-javascript-reset-game-screen
+//probably incorrect
+// location.reload();
+
+// also this
