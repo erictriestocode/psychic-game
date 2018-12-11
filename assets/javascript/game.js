f252ec2c70041516428b52b1f5e6725d9d2eee0a
erictriestocode <43325093+erictriestocode@users.noreply.github.com>
2018-12-10 20:26:12 -0600

    Initial mass commit(file structure and base code)

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+// Javascript file for Psychic game
+
+// User Guesses
+var userGuess;
+var computerGuess;
+// tracking wins/losses
+var wins = 0;
+var losses = 0;
+// tracking guesses
+var guessList = [];
+
+
+// computer choice array
+var compChoice = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z];
+
+
